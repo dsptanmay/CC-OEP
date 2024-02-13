64e0986c56345e658a46fc27f909e60a15e2a705
abhishekkalgudi <abhishekkalgudi03@gmail.com>
2024-02-13 11:51:44 +0530

    user_specific_get_implemented

--- app/api/recipes/[uid]/route.js
+++ app/api/recipes/[uid]/route.js
@@ -1,15 +1,34 @@
 import mongoose from "mongoose";
 import { NextRequest, NextResponse } from "next/server";
-import Recipe from '@/app/models/recipe';
+import Recipe from "@/app/models/recipe";
 
 export async function POST(request, { params }) {
-    try {
-        const { uid: userid } = params;
-        const { title, description, servings, rating, steps } = await request.json();
-        const savedData = await Recipe.create({userid, title, description, servings, rating, steps});
-        console.log("data saved");
-        return NextResponse.json({message: savedData}, { status: 201});
-    } catch (error) {
-        return NextResponse.json({ message: error}, { status: 500});
-    }
+  try {
+    const { uid: userid } = params;
+    const { title, description, servings, rating, steps } =
+      await request.json();
+    const savedData = await Recipe.create({
+      userid,
+      title,
+      description,
+      servings,
+      rating,
+      steps,
+    });
+    console.log("data saved");
+    return NextResponse.json({ message: savedData }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
+}
+
+export async function GET(request, { params }) {
+  try {
+    const { uid: userid } = params;
+    const allData = await Recipe.find({});
+    const particularUserData = allData.filter((data) => data.userid == userid);
+    return NextResponse.json({ message: particularUserData }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  }
 }
